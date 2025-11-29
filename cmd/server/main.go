0a905ef71fbb6243444658c3c13f252ee2e8df76
sunnyaxin <sunnyyaxin@gmail.com>
2025-11-29 22:01:28 +0800

    refactor meter provider.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"github.com/gogf/gf/contrib/metric/otelmetric/v2"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
@@ -13,15 +14,7 @@ func main() {
 	var ctx = gctx.New()
 	// Set custom JSON logging handler to match google structured logging format.
 	glog.SetDefaultHandler(LoggingJsonHandler)
-
-	// Set up OpenTelemetry Prometheus exporter to export metric as prometheus format.
-	exporter, _ := prometheus.New()
-	// OpenTelemetry provider.
-	provider := otelmetric.MustProvider(
-		otelmetric.WithReader(exporter),
-	)
-	provider.SetAsGlobal()
-	defer provider.Shutdown(ctx)
+	providerShutdown, _ := initMeterProvider()
 
 	s := g.Server()
 	s.BindHandler("/hello", func(r *ghttp.Request) {
@@ -32,4 +25,17 @@ func main() {
 	s.BindHandler("/metrics", otelmetric.PrometheusHandler)
 	s.SetPort(8080)
 	s.Run()
+
+	defer providerShutdown(ctx)
+}
+
+func initMeterProvider() (func(context.Context) error, error) {
+	// Set up OpenTelemetry Prometheus exporter to export metric as prometheus format.
+	exporter, _ := prometheus.New()
+	// OpenTelemetry provider.
+	provider := otelmetric.MustProvider(
+		otelmetric.WithReader(exporter),
+	)
+	provider.SetAsGlobal()
+	return provider.Shutdown, nil
 }
